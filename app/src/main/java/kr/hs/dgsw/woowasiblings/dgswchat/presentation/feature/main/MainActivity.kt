9e876c166f75a00be0f35e95bb8cfff2e7becc9c
윤석규 <devSxxkgyu@gmail.com>
2023-02-07 17:32:54 +0900

    Modify :: main

---
+++ app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/presentation/feature/main/MainActivity.kt
@@ -0,0 +1,28 @@
+package kr.hs.dgsw.woowasiblings.dgswchat.presentation.feature.main
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kr.hs.dgsw.woowasiblings.dgswchat.R
+import kr.hs.dgsw.woowasiblings.dgswchat.databinding.ActivityMainBinding
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.base.BaseActivity
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.feature.chat.ChatFragment
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
+
+    override val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun start() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, ChatFragment())
+            .commit()
+    }
+
+}
---
+++ app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/presentation/feature/main/MainViewModel.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.woowasiblings.dgswchat.presentation.feature.main
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+
+): BaseViewModel() {
+
+}
