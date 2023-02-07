a16a4d2f83da903740e452c32d29a4ab50a56822
윤석규 <devSxxkgyu@gmail.com>
2023-02-07 20:11:09 +0900

    Add :: Sharedpreferences

--- app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/App.kt
+++ app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/App.kt
@@ -2,6 +2,16 @@ package kr.hs.dgsw.woowasiblings.dgswchat
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kr.hs.dgsw.woowasiblings.dgswchat.data.util.PreferenceManager
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+    companion object {
+        lateinit var prefs: PreferenceManager
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        prefs = PreferenceManager(applicationContext)
+    }
+}
