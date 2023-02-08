57087de435bc40807bf0a26610b858236f285410
윤석규 <devSxxkgyu@gmail.com>
2023-02-08 11:01:13 +0900

    Add :: 질문 등록 기능 추가

--- app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/presentation/feature/add/AddFragment.kt
+++ app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/presentation/feature/add/AddFragment.kt
@@ -1,6 +1,54 @@
 package kr.hs.dgsw.woowasiblings.dgswchat.presentation.feature.add
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.hs.dgsw.woowasiblings.dgswchat.R
+import kr.hs.dgsw.woowasiblings.dgswchat.databinding.FragmentAddBinding
 import kr.hs.dgsw.woowasiblings.dgswchat.presentation.base.BaseFragment
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.feature.chat.ChatViewModel
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.utils.extension.repeatOnStarted
 
-class AddFragment: BaseFragment<>() {
+@AndroidEntryPoint
+class AddFragment: BaseFragment<FragmentAddBinding, AddViewModel>(R.layout.fragment_add) {
+
+    override val viewModel: AddViewModel by viewModels()
+    private var tag: String? = null
+
+    override fun start() {
+        with(mViewModel) {
+            repeatOnStarted { eventFlow.collect { event -> handleEvent(event) } }
+
+            binding.apply {
+                btnSubmit.setOnClickListener {
+                    tag?.let {
+                        submitPost(etTitle.text.toString(), tag!!, etContent.text.toString())
+                    }
+
+                }
+
+                schoolTag.setOnClickListener {
+                    tag = "Tech"
+                    schoolTag.setCardBackgroundColor(resources.getColor(R.color.chat))
+                    tvSchool.setTextColor(resources.getColor(R.color.main))
+                    techTag.setCardBackgroundColor(resources.getColor(R.color.gray2))
+                    tvTech.setTextColor(resources.getColor(R.color.gray))
+                }
+
+                techTag.setOnClickListener {
+                    tag = "School"
+                    schoolTag.setCardBackgroundColor(resources.getColor(R.color.gray2))
+                    tvSchool.setTextColor(resources.getColor(R.color.gray))
+                    techTag.setCardBackgroundColor(resources.getColor(R.color.chat))
+                    tvTech.setTextColor(resources.getColor(R.color.main))
+                }
+            }
+        }
+    }
+
+    private fun handleEvent(event: AddViewModel.Event): Any =
+        when (event) {
+            is AddViewModel.Event.SuccessSubmit -> requireActivity().supportFragmentManager.popBackStack()
+            is AddViewModel.Event.UnkownException -> shortToast("알 수 없는 오류가 발생했습니다.")
+        }
 }
--- app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/presentation/feature/add/AddViewModel.kt
+++ app/src/main/java/kr/hs/dgsw/woowasiblings/dgswchat/presentation/feature/add/AddViewModel.kt
@@ -1,4 +1,43 @@
 package kr.hs.dgsw.woowasiblings.dgswchat.presentation.feature.add
 
-class AddViewModel {
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.hs.dgsw.woowasiblings.dgswchat.domain.model.post.PostDto
+import kr.hs.dgsw.woowasiblings.dgswchat.domain.repository.PostRepository
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.base.BaseViewModel
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.utils.MutableEventFlow
+import kr.hs.dgsw.woowasiblings.dgswchat.presentation.utils.asEventFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class AddViewModel @Inject constructor(
+    private val postRepository: PostRepository
+) : BaseViewModel() {
+
+    private val _eventFLow = MutableEventFlow<Event>()
+    val eventFlow = _eventFLow.asEventFlow()
+
+    fun submitPost(title: String, tag: String, content: String) = viewModelScope.launch(Dispatchers.IO) {
+        kotlin.runCatching {
+            postRepository.submitPost(
+                PostDto(title, tag, content)
+            )
+        }.onSuccess {
+            event(Event.SuccessSubmit)
+        }.onFailure {
+            event(Event.UnkownException)
+        }
+    }
+    private fun event(event: Event) {
+        viewModelScope.launch {
+            _eventFLow.emit(event)
+        }
+    }
+
+    sealed class Event {
+        object SuccessSubmit: Event()
+        object UnkownException: Event()
+    }
 }
